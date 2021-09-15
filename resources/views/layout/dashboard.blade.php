543628f8420ea889d4c0f0f160d957154c6c3442
vishal <vishal@prolitus.com>
2021-09-15 18:56:32 +0530

    logo update

--- resources/views/layout/dashboard.blade.php
+++ resources/views/layout/dashboard.blade.php
@@ -42,7 +42,8 @@
 
         <!-- Preloader -->
         <div class="preloader flex-column justify-content-center align-items-center">
-            <img class="animation__shake" src="{{asset('public/assets/dist/img/AdminLTELogo.png')}}" alt="AdminLTELogo" height="60" width="60">
+            <center></center>
+            <img class="animation__shake" src="{{asset('public/images/logo.jpg')}}" alt="AdminLTELogo" height="60" width="auto">
         </div>
 
         <!-- Navbar -->
